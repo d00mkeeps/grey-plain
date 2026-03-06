4364efabd0732bfbdcb94bedf8e43f82a151bfde
Miles  Hillary <miles.i.hillary@gmail.com>
2026-03-06 13:06:32 +0000

    chore: remove unnecessary test and debug files from tracking

--- cognitive-trajectory/test_geometry.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const brain = JSON.parse(fs.readFileSync('cognitive-trajectory/public/brain.json', 'utf8'));
-
-console.log('first 10 region indices:');
-console.log(brain.vertices.slice(0, 10).map(v => v.region));
--- cognitive-trajectory/test_material.mjs
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-const mat = new THREE.MeshPhongMaterial({
-  color: 0xffffff,
-  vertexColors: true
-});
-console.log('Phong with vertexColors has color:', mat.color);
